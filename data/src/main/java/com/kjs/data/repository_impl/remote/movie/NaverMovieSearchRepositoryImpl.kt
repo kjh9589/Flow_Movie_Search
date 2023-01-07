f3c468195bfb00aec7086a0f4fa581dd7a1b02e7
kjh9589 <jeonghwan9589@gmail.com>
2023-01-08 00:40:26 +0900

    fix: 검색어 결과 없을 시 토스트 메시지 띄우도록 수정

--- data/src/main/java/com/kjs/data/repository_impl/remote/movie/NaverMovieSearchRepositoryImpl.kt
+++ data/src/main/java/com/kjs/data/repository_impl/remote/movie/NaverMovieSearchRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.kjs.data.repository_impl.remote.movie
 
+import android.content.Context
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
@@ -14,14 +15,16 @@ import javax.inject.Inject
 
 class NaverMovieSearchRepositoryImpl @Inject constructor(
     private val api: NaverSearchMovieService,
-    @IoDispatcher private val dispatcher: CoroutineDispatcher
+    @IoDispatcher private val dispatcher: CoroutineDispatcher,
+    private val context: Context
 ): NaverMovieSearchRepository {
     override suspend fun searchNaverMovieList(keyword: String): Flow<PagingData<MovieModel>> {
         return Pager(PagingConfig(RemoteNaverSearchMoviePagingSource.DISPLAY_COUNT)) {
             RemoteNaverSearchMoviePagingSource(
                 dispatcher = dispatcher,
                 api = api,
-                keyword = keyword
+                keyword = keyword,
+                context = context
             )
         }.flow
     }
